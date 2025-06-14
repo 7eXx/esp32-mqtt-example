f4804d2daf954373e72e7b9bbb557bad5e06379b
Texx <marco.tex@live.com>
2025-06-14 12:42:12 +0200

    chore: external config header for  params.

---
+++ config.h
@@ -0,0 +1,8 @@
+// Wi-Fi credentials
+const char* ssid = "TP-LINK_EA32";
+const char* password = "11642251";
+
+// MQTT broker info
+const char* mqtt_server = "192.168.1.95"; // You can use test.mosquitto.org, etc.
+const int mqtt_port = 1883;
+const char* mqtt_topic = "esp32/test";
--- esp32-mqtt-example.ino
+++ esp32-mqtt-example.ino
@@ -1,14 +1,6 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
-
-// Wi-Fi credentials
-const char* ssid = "TP-LINK_EA32";
-const char* password = "11642251";
-
-// MQTT broker info
-const char* mqtt_server = "192.168.1.95"; // You can use test.mosquitto.org, etc.
-const int mqtt_port = 1883;
-const char* mqtt_topic = "esp32/test";
+#include "config.h"
 
 WiFiClient espClient;
 PubSubClient client(espClient);
